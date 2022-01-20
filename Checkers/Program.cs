a6fb5933b602dd1c256ead38c74fcd2ae40c7ab0
Kamil <kamil271e@gmail.com>
2022-01-20 13:20:19 +0100

    BugFix & util functions

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -4,10 +4,11 @@ namespace Checkers
 {
     class Game
     {
-        public int[,] board;
+        public int[,] board; // plansza
         // black - czarne pionki, white - białe pionki, -1 - białe pole (nie używane), 0 - puste czarne pole
         int black = 2, white = 1;
         int blackQueen = 4, whiteQueen = 3; // damki w odpowiednich kolorach
+        int pawn; // aktualnie wybrany pionek
         static int order = 0; // zmienna ktora bedzie okreslala kolejnosc gry, jesli order % 2 == 0 gra bialy w przeciwnym wypadku czarny,
                               // zmienna bedzie aktualizowana z kazdym poprawnym wywolaniem funkcji move - rozgrywke rozpoczynaja biale
         public Game() // konstruktor tworzacy plansze startowa, przy utworzeniu nowej instancji klasy
@@ -63,7 +64,6 @@ namespace Checkers
         public void Move(int i0, int j0, int i, int j) // (i0,j0) wspolrzedne wybranego pionka, (i,j) wspolrzedne docelowe
         {
             int flag = 0;
-            int pawn;
             if (order % 2 == 0) pawn = white;
             else pawn = black;
             try
@@ -77,7 +77,7 @@ namespace Checkers
                     flag = 1; goto error;
                 }
                 
-                // WYBOR ZLEGO POLA DO PORUSZENIA SIE - ZAJETE LUB NIEUZYWANE W GRZE
+                // WYBOR ZLEGO POLA DOCELOWEGO - ZAJETE LUB NIEUZYWANE W GRZE
                 else if (board[i,j] != 0)
                 {
                     if (board[i,j] != -1) { flag = 3; goto error; }
@@ -90,12 +90,7 @@ namespace Checkers
                     if (Math.Abs(i - i0) == Math.Abs(j - j0))
                     {
                         int mv_i = 0, mv_j = 0; // zmienne okreslajace kierunek poruszania sie damki
-                        // 4 mozliwosci :
-
-                        if (i0 > i && j0 > j) { mv_i = -1; mv_j = -1; } // lewo gora // poruszamy sie zmniejszajac 'i' i zmniejszajac 'j'
-                        else if (i0 > i && j0 < j) { mv_i = -1; mv_j = 1; } // prawo gora // poruszamy sie zmniejszajac 'i' i zwiekszajac 'j'
-                        else if (i0 < i && j0 < j) { mv_i = 1; mv_j = 1; } // prawo dol // poruszamy sie zwiekszajac 'i' i zwiekszajac 'j'
-                        else if (i0 < i && j0 < j) { mv_i = 1; mv_j = -1; } // lewo dol // poruszamy sie zwiekszajac 'i' i zmniejszajac 'j'
+                        InitDirVariablesQueen(ref mv_i, ref mv_j, i0, j0, i, j);
 
                         int k;
                         for (k = 1; k < Math.Abs(i - i0) - 1; k++)
@@ -107,26 +102,17 @@ namespace Checkers
                         // opponent to puste pole - wykonujemy zwykly ruch
                         if (opponent == 0)
                         {
-                            board[i, j] = board[i0, j0];
-                            board[i0, j0] = 0;
-                            order++;
-                            Console.WriteLine("damka bez bicia");
-                            Console.WriteLine($"{i0 + mv_i * k} - { j0 + mv_j * k}");
-                            Console.WriteLine($"{board[i0 + mv_i * k, j0 + mv_j * k]}");
+                            Capture(i0, j0, i, j, -1, -1);
                         }
                         else
                         {
-                            // sprawdzenie czy w odpowiednim nie stoi czasem nasz wlasny pionek
+                            // sprawdzenie czy w odpowiednim miejscu nie stoi czasem nasz wlasny pionek
                             if ((board[i0, j0] == whiteQueen && opponent != black && opponent != blackQueen) || (board[i0, j0] == blackQueen && opponent != white && opponent != whiteQueen))
                             {
                                 flag = 2; goto error;
                             }
                             // bicie
-                            board[i0 + mv_i * k, j0 + mv_j * k] = 0;
-                            board[i, j] = board[i0, j0];
-                            board[i0, j0] = 0;
-                            order++;
-                            Console.WriteLine("damka z biciem");
+                            Capture(i0, j0, i, j, i0 + mv_i * k, j0 + mv_j * k);
                         }
                     }
                     else
@@ -135,33 +121,25 @@ namespace Checkers
                     }
                 }
                 
+                // PODWOJNE BICIE
+                else if (Math.Abs(i0-i) == 4 && (Math.Abs(j0-j) == 0 || Math.Abs(j0-j) == 4))
+                {
+                    //
+                }
+                
                 // POJEDYNCZE BICIE
                 else if ((Math.Abs(j0 - j) == 2) && ((pawn == white && i0 - i == 2) || (pawn == black && i - i0 == 2)))
                 {
-                    if (board[i, j] == 0)
-                    {
-                        int mv_i = 0, mv_j = 0; // zmienne pomocnicze pozwalające określić gdzie jest atakowany pionek
-                        if (pawn == white && j > j0) { mv_i = -1; mv_j = 1; }
-                        else if (pawn == white && j < j0) { mv_i = -1; mv_j = -1; }
-                        else if (pawn == black && j > j0) { mv_i = 1; mv_j = 1; }
-                        else if (pawn == black && j < j0) { mv_i = 1; mv_j = -1; }
+                    int mv_i = 0, mv_j = 0; // zmienne pomocnicze pozwalające określić gdzie jest atakowany pionek
+                    InitDirVariables(ref mv_i, ref mv_j, i0, j0, i, j);
 
-                        int opponent = board[i0 + mv_i, j0 + mv_j];
-                        // sprawdzenie czy na pewno przeciwnik znajduje sie w dobrym miejscu
-                        if ((pawn == white && (opponent == black || opponent == blackQueen)) || (pawn == black && (opponent == white || opponent == whiteQueen)))
-                        {
-                            board[i0 + mv_i, j0 + mv_j] = 0; // usuwanie atakowanego pionka
-                            board[i, j] = board[i0, j0];
-                            board[i0, j0] = 0;
-                            order++;
-                            if (pawn == white && i == 0) board[i, j] = whiteQueen;
-                            else if (pawn == black && i == 7) board[i, j] = blackQueen;
-                            Console.WriteLine("bicie pionkiem");
-                        }
-                        else
-                        {
-                            flag = 2; goto error;
-                        }
+                    int opponent = board[i0 + mv_i, j0 + mv_j];
+                    // sprawdzenie czy na pewno przeciwnik znajduje sie w dobrym miejscu
+                    if ((pawn == white && (opponent == black || opponent == blackQueen)) || (pawn == black && (opponent == white || opponent == whiteQueen)))
+                    {
+                        Capture(i0, j0, i, j, i0 + mv_i, j0 + mv_j);
+                        if (pawn == white && i == 0) board[i, j] = whiteQueen;
+                        else if (pawn == black && i == 7) board[i, j] = blackQueen;
                     }
                     else
                     {
@@ -178,12 +156,9 @@ namespace Checkers
                 // ZWYKŁY RUCH
                 else
                 {
-                    board[i, j] = board[i0, j0];
-                    board[i0, j0] = 0;
-                    order++;
+                    Capture(i0, j0, i, j, -1, -1);
                     if (pawn == white && i == 0) board[i, j] = whiteQueen;
                     else if (pawn == black && i == 7) board[i, j] = blackQueen;
-                    Console.WriteLine("zwykly ruch");
                 }
             }
             catch
@@ -222,6 +197,29 @@ namespace Checkers
             else name = "czarne";
             Console.WriteLine($"Koniec gry! Wygrywają pionki {name}.");
             return true;
+        }
+        public void InitDirVariablesQueen(ref int mv_i, ref int mv_j, int i0, int j0, int i, int j)
+        {
+            // 4 mozliwosci :
+            if (i0 > i && j0 > j) { mv_i = -1; mv_j = -1; } // lewo gora // poruszamy sie zmniejszajac 'i' i zmniejszajac 'j'
+            else if (i0 > i && j0 < j) { mv_i = -1; mv_j = 1; } // prawo gora // poruszamy sie zmniejszajac 'i' i zwiekszajac 'j'
+            else if (i0 < i && j0 < j) { mv_i = 1; mv_j = 1; } // prawo dol // poruszamy sie zwiekszajac 'i' i zwiekszajac 'j'
+            else if (i0 < i && j0 > j) { mv_i = 1; mv_j = -1; } // lewo dol // poruszamy sie zwiekszajac 'i' i zmniejszajac 'j'
+
+        }
+        public void InitDirVariables(ref int mv_i, ref int mv_j, int i0, int j0, int i, int j)
+        {
+            if (pawn == white && j > j0) { mv_i = -1; mv_j = 1; }
+            else if (pawn == white && j < j0) { mv_i = -1; mv_j = -1; }
+            else if (pawn == black && j > j0) { mv_i = 1; mv_j = 1; }
+            else if (pawn == black && j < j0) { mv_i = 1; mv_j = -1; }
+        }
+        public void Capture(int i0, int j0, int i, int j, int ix, int jx)
+        {
+            if (ix != -1 && jx != -1) board[ix, jx] = 0; // usuwanie atakowanego pionka
+            board[i, j] = board[i0, j0];
+            board[i0, j0] = 0;
+            order++;
         }
     }
     class Program
